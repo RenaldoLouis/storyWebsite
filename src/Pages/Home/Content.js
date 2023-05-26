82f0ace4c151c26f97ca148899b80e0f2ec14e29
RenaldoLouis <renald@10xers.co>
2023-05-26 09:56:33 +0700

    fix bootstrap so modal can launch

--- src/Pages/Home/Content.js
+++ src/Pages/Home/Content.js
@@ -2,18 +2,21 @@ import React, {useEffect, useState} from "react";
 import homeCard from "../../assets/images/spiderman.jpg"
 import {collection, addDoc, getDocs} from "firebase/firestore";
 import {db} from '../../firebase';
+import data from "../../data/content.json"
+import {toast} from 'react-toastify';
+
 
 const Content = () => {
-    const [todo, setTodo] = useState("");
-    const [todos, setTodos] = useState([]);
+    const [name, setName] = useState("");
+    const [comment, setComment] = useState("");
+    const [datas, setDatas] = useState([]);
 
     const fetchPost = async () => {
         await getDocs(collection(db, "comments"))
             .then((querySnapshot) => {
                 const newData = querySnapshot.docs
                     .map((doc) => ({...doc.data(), id: doc.id}));
-                setTodos(newData);
-                console.log(todos, newData);
+                setDatas(newData);
             })
 
     }
@@ -22,54 +25,76 @@ const Content = () => {
         fetchPost();
     }, [])
 
-    const addTodo = async (e) => {
+    const handleAddComment = async (e) => {
         e.preventDefault();
 
         try {
-            const docRef = await addDoc(collection(db, "comments"), {
-                comment: todo,
+            await addDoc(collection(db, "comments"), {
+                name,
+                comment,
             });
-            console.log("Document written with ID: ", docRef.id);
+            toast.success("Wow so easy!")
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     }
+
+    const handleReply = async (e) => {
+        e.preventDefault();
+
+        try {
+            await addDoc(collection(db, "comments"), {
+                name,
+                comment,
+            });
+            toast.success("Wow so easy!")
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+
+
     return (
         <>
-            <img className="home-coverImage" src={homeCard} alt="Italian Trulli" />
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        1 of 2
-                    </div>
-                    <div class="col">
-                        2 of 2
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        1 of 3
-                    </div>
-                    <div class="col">
-                        2 of 3
-                    </div>
-                    <div class="col">
-                        3 of 3
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col align-self-start">
-                        One of three columns
-                    </div>
-                    <div class="col align-self-center">
-                        One of three columns
-                    </div>
-                    <div class="col align-self-end">
-                        One of three columns
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <img className="home-coverImage" src={homeCard} alt="Italian Trulli" />
+            <div class="container">
+                <div>
+                    {data.paragraph1}
+                </div>
+            </div>
+
+            <video style={{width: "50vw"}} controls width="100%">
+                <source src="/video1.mp4" type="video/mp4"
+                />
+                Sorry, your browser doesn't support videos.
+            </video>
+
+            <audio controls>
+                <source src="/audio1.mp3" type="audio/mpeg" />
+                Your browser does not support the audio element.
+            </audio>
 
             <section className="todo-container">
                 <div className="todo">
@@ -80,37 +105,59 @@ const Content = () => {
                     <div>
 
                         <div>
-                            <input
-                                type="text"
-                                placeholder="What do you have to do today?"
-                                onChange={(e) => setTodo(e.target.value)}
-                            />
+                            <form>
+                                <label style={{display: "grid"}}>
+                                    Enter your name:
+                                    <input
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        style={{width: "200px"}}
+                                    />
+                                </label>
+                                <label style={{display: "grid"}}>
+                                    Enter your Comment:
+                                    <textarea
+                                        value={comment}
+                                        onChange={(e) => setComment(e.target.value)}
+                                        style={{width: "50vw"}}
+                                    />
+                                </label>
+                            </form>
                         </div>
 
                         <div className="btn-container">
                             <button
                                 type="submit"
                                 className="btn"
-                                onClick={addTodo}
+                                onClick={handleAddComment}
                             >
                                 Submit
                             </button>
                         </div>
 
                     </div>
-
-                    <div className="todo-content">
-                        ...
-                    </div>
                 </div>
-            </section>
+            </section >
 
             <div className="todo-content">
                 {
-                    todos?.map((todo, i) => (
-                        <p key={i}>
-                            {todo.comment}
-                        </p>
+                    datas?.map((data, i) => (
+                        <div>
+                            <p key={i}>
+                                {data.name}
+                            </p>
+                            <p key={i}>
+                                {data.comment}
+                            </p>
+                            <button
+                                type="submit"
+                                className="btn"
+                                onClick={handleReply}
+                            >
+                                Reply
+                            </button>
+                        </div>
                     ))
                 }
             </div>
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './styles/main.scss'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
